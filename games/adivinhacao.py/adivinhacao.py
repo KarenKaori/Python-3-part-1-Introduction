f7ace49dfb4582119a26e3c19640d552839a0346
Karen Harada <myacademy.learning1@gmail.com>
2021-09-07 13:25:40 -0300

    Loop using while

--- games/adivinhacao.py/adivinhacao.py
+++ games/adivinhacao.py/adivinhacao.py
@@ -3,11 +3,12 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
-allowed_attempts = 10
-user_attempts = 1
+allowed_attempts = 3
+rodada = 1
 
-while user_attempts <= allowed_attempts:
-
+while rodada <= allowed_attempts:
+    print(f"Tentativa {rodada} de {allowed_attempts}")
+    #print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu número: ")
     print("Você digitou: ", chute_str)
     # print("Type: ", type(chute))
@@ -26,6 +27,6 @@ while user_attempts <= allowed_attempts:
             print("Você errou, o seu chute foi menor que o numero secreto.")
         elif maior:
             print("Você errou, o seu chute foi maior que o numero secreto.")
-        user_attempts = user_attempts + 1
+        rodada = rodada + 1
 
 print("Fim do jogo")
